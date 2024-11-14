695bba234baf9e7b28937648f0aacd1fd4f1f76b
Miko <200107017@stu.sdu.edu.kz>
2024-11-15 03:11:44 +0500

    change1

--- backend/settings.py
+++ backend/settings.py
@@ -136,7 +136,7 @@ DEFAULT_FROM_EMAIL = '200107017@stu.sdu.edu.kz'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # Make sure this is correct
 ]
